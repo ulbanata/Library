ba995ee5b774348ac5273fd10c9abe06c5968ac1
Patrick Brennan <chief125@gmail.com>
2014-02-07 13:54:59 -0600

    Currently on adding new books to the library and assigning it an id

--- library.rb
+++ library.rb
@@ -1,24 +1,56 @@
 
 class Book
   attr_reader :author
+  attr_reader :title
+  attr_reader :id
+  attr_reader :status
 
-  def initialize(title, author)
+  def initialize(title="The Stranger", author="Albert Camus")
     @author = author
+    @title = title
+    @id = nil
+    @status = 'available'
   end
+
+  def check_out
+    if @status == 'available'
+      @status = 'checked_out'
+      return true
+    else
+      return false
+    end
+  end
+
+  def check_in
+    @status = 'available'
+  end
+
+
 end
 
 class Borrower
+  attr_reader :name
   def initialize(name)
+    @name = name
   end
 end
 
 class Library
-  def initialize(name)
+  attr_reader :books
+  def initialize
     @books = []
+    @count = 0
   end
 
-  def books
+  def register_new_book(name, author)
+    @books << Book.new(name, author)
   end
+
+  # def books
+  #   def count
+  #     @count += 1
+  #   end
+  # end
 
   def add_book(title, author)
   end
