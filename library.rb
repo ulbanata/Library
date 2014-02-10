1d055a967bb7a3fd30c4168c4501ac365134870b
Patrick Brennan <chief125@gmail.com>
2014-02-09 19:37:55 -0600

    Added a due date system. Can't check out a book if borrower currently has an overdue book

--- library.rb
+++ library.rb
@@ -1,3 +1,4 @@
+require 'date'
 
 class Book
   attr_reader :author
@@ -5,6 +6,7 @@ class Book
   attr_reader :id
   attr_reader :status
   attr_reader :borrower
+  attr_reader :due_date
 
   @@count = 0
 
@@ -15,23 +17,34 @@ class Book
     @@count += 1
     @id = @@count
     @borrower = []
+    @due_date = nil
   end
 
-  def check_out(borrower)
-    puts @status
-    if @status == 'available' && borrower.checked_out < 2
+  def check_out(borrower_obj)
+    overdue = check_overdue(borrower_obj)
+    cur_date = Time.new
+    if @status == 'available' && borrower_obj.checked_out.length < 2 && !overdue
       @status = 'checked_out'
-      @borrower << borrower
-      @borrower.first.checked_out += 1
+      @borrower << borrower_obj
+      @due_date = Time.now + 604800
       return true
     else
       return false
     end
   end
 
+  def check_overdue(borrower_obj)
+    cur_date = Time.now
+    borrower_obj.checked_out.each do |book|
+      return true if (book.due_date - cur_date) <= 0
+    end
+    return false
+  end
+
+
   def check_in
     @status = 'available'
-    @borrower.first.checked_out -= 1
+    return @borrower
     @borrower = []
   end
 
@@ -42,7 +55,7 @@ class Borrower
   attr_reader :name
   attr_accessor :checked_out
   def initialize(name)
-    @checked_out = 0
+    @checked_out = []
     @name = name
   end
 end
@@ -69,32 +82,33 @@ class Library
   # end
 
   def check_out_book(book_id, borrower)
-    @books.each do |x|
-      puts @books
-      if x.id == book_id && x.check_out(borrower)
-        return x
+    @books.each do |book|
+      if book.id == book_id && book.check_out(borrower)
+        borrower.checked_out << book
+        return book
       end
     end
     return nil
   end
 
   def get_borrower(book_id)
-    @books.each do |x|
-      if x.id == book_id
-        return x.borrower.first.name
+    @books.each do |book|
+      if book.id == book_id
+        return book.borrower.first.name
       end
     end
   end
 
   def check_in_book(book)
-    book.check_in
+    borrower = book.check_in
+    borrower.delete(book)
   end
 
   def available_books
-    @books.select { |x| x.status == 'available' }
+    @books.select { |book| book.status == 'available' }
   end
 
   def borrowed_books
-    @books.select { |x| x.status == 'checked_out' }
+    @books.select { |book| book.status == 'checked_out' }
   end
 end
